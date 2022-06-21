0c373ce8fa608b740bec706eea1f5c9b97bfbd89
darwin cordova <dcordova@multileaf.io>
2022-06-21 16:06:27 -0500

    subiendo los ultimos detallles

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+document.getElementById('btn1').addEventListener('click',function(){
+    body = document.getElementById('cuerpo');
+    body.style.background = 'rgba(146,196,252,1)';
+    
+});
+document.getElementById('btn2').addEventListener('click',function(){
+    body = document.getElementById('cuerpo');
+    body.style.background = 'rgba(30,58,138,1)';
+    
+});
+
